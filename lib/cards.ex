180a8fc66936be6f108db6cfd3a8880765579e99
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2022-05-24 00:49:26 -0300

    section/testing-and-documentation:
    commit 51287e9872a06a6d08cd130c36919ab92d23e4af
    Author: Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
    Date:   Tue May 24 00:48:13 2022 -0300
    
        add test cases for create_deck and shuffle
    
    commit bbd0b9b6031cc0ba0aefaf1eab0abfc2f369c72a
    Author: Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
    Date:   Tue May 24 00:37:14 2022 -0300
    
        add documentation to method contains?
    
    commit 5df8d11c9ae7a579fa92a19dcf1863fc68733b84
    Author: Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
    Date:   Tue May 24 00:20:51 2022 -0300
    
        add documentation for methods create_deck and deal
    
    commit 7aca562146ff7de00eea419f04dc898ce3b2a189
    Author: Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
    Date:   Tue May 24 00:07:52 2022 -0300
    
        update Cards moduledoc

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,8 +1,11 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for `Cards`.
+    Provides methods for creating and handling a deck of cards.
   """
 
+  @doc """
+    Returns a list of strings representing a deck of playing cards.
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -16,10 +19,35 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+      iex> Cards.contains?(deck, "Joker")
+      false
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder or the deck.
+    The `hand_size` argument indicates how manyy cards should
+    be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
