23bc9c8f87bf26ced5ecdebeef88f07e7bd9160b
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2022-05-23 21:51:33 -0300

    section/pattern-matching:
    commit 6db74968e16a8ddac6ae909071e8671e71315e44
    Author: Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
    Date:   Mon May 23 21:50:09 2022 -0300
    
        install :ex_doc
    
    commit 03bc0391e468f325f1684007335b7441f7e8cde1
    Author: Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
    Date:   Mon May 23 21:40:30 2022 -0300
    
        add method create_hand
    
    commit 7b25faf07b98a07ee5f218da9a06c2279c742b5e
    Author: Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
    Date:   Mon May 23 21:28:18 2022 -0300
    
        update load method
    
    commit 1fe0c3a284a75b6a5c2f5be912d6247164dcb2bd
    Author: Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
    Date:   Mon May 23 21:10:42 2022 -0300
    
        add load method
    
    commit 0809aafd0ea4f993b4208e9ce9e4b070db8a2a24
    Author: Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
    Date:   Mon May 23 20:57:54 2022 -0300
    
        add save method

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,19 +3,6 @@ defmodule Cards do
   Documentation for `Cards`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    "hello world!"
-  end
-
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -35,5 +22,23 @@ defmodule Cards do
 
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist!"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
